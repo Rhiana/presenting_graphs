133cc605b7d4b695d8de1af09a3ec44d7756b20c
Rhiana Heath <rhiana.heath@reinteractive.net>
2018-06-13 17:03:57 +1000

    moving bar graph logic to presenter

---
+++ app/presenters/bar_presenter.rb
@@ -0,0 +1,37 @@
+class BarPresenter
+  attr_reader :score, :total, :height, :width
+
+  def initialize(view, score, total, height, width)
+    @view     = view
+    @score    = score
+    @total    = total
+    @height   = height
+    @width    = width
+  end
+
+  def render
+    # Renders the bar_graphs/_bar_graph_svg.html.erb partial,
+    # passing in this presenter as a `graph` variable
+    @view.render("bar_graphs/bar_graph_svg", graph: self)
+  end
+
+  def percentage
+    (score / total.to_f) * 100
+  end
+
+  def fill
+    (score / total.to_f) * height
+  end
+
+  def y_coord
+    height - fill
+  end
+
+  def y_text_coord
+    y_coord + 25
+  end
+
+  def x_text_coord
+    width / 2.0
+  end
+end
