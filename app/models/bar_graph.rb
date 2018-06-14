c5a4ed49af1a1ec367bc127edc0c7f4f3a530cd2
Rhiana Heath <rhiana.heath@reinteractive.net>
2018-06-14 10:42:34 +1000

    adding in some basic validation and errors

--- app/models/bar_graph.rb
+++ app/models/bar_graph.rb
@@ -1,2 +1,5 @@
 class BarGraph < ApplicationRecord
+  self.table_name = "bar_graphs"
+
+  validates :score, :total, presence: true, allow_nil: false
 end
--- app/models/circle_graph.rb
+++ app/models/circle_graph.rb
@@ -1,2 +1,5 @@
 class CircleGraph < ApplicationRecord
+  self.table_name = "circle_graphs"
+
+  validates :score, :total, presence: true, allow_nil: false
 end
