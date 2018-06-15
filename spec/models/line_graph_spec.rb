4677d4fc950a72aa5e547a382e60a9661b482831
Rhiana Heath <rhiana.heath@reinteractive.net>
2018-06-15 13:43:04 +1000

    updated db for values under 50 only

--- spec/models/line_graph_spec.rb
+++ spec/models/line_graph_spec.rb
@@ -12,10 +12,13 @@ RSpec.describe LineGraph, type: :model do
       is_expected.to validate_presence_of(:score3)
     end
 
-    it "has three scores which are numbers" do
-      is_expected.to validate_numericality_of(:score1)
-      is_expected.to validate_numericality_of(:score2)
-      is_expected.to validate_numericality_of(:score3)
+    it "has three scores which are numbers from 0 to 50" do
+      should validate_numericality_of(:score1).
+                 is_less_than_or_equal_to(50)
+      should validate_numericality_of(:score2).
+                 is_less_than_or_equal_to(50)
+      should validate_numericality_of(:score3).
+                 is_less_than_or_equal_to(50)
     end
   end
 end
