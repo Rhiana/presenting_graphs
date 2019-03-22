018ac097cbf007f5bc35771a96b59ba477b9b9a2
Rhiana Heath <rhiana.heath@reinteractive.net>
2019-03-22 11:48:28 +1100

    updating ruby gems

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,27 +1,27 @@
 FactoryBot.define do
   factory :pie_graph do
-    score1 1
-    score2 1
-    score3 1
+    score1 { 1 }
+    score2 { 1 }
+    score3 { 1 }
   end
   factory :gauge_graph do
-    score 1
-    total 1
+    score { 1 }
+    total { 1 }
   end
   factory :line_graph do
-    score1 1
-    score2 1
-    score3 1
+    score1 { 1 }
+    score2 { 1 }
+    score3 { 1 }
   end
 
   factory :circle_graph do
-    score 10
-    total 20
+    score { 1 }
+    total { 1 }
   end
 
   factory :bar_graph do
-    score1 1
-    score2 1
+    score1 { 1 }
+    score2 { 1 }
   end
 
 end
