ba1924861a28d64ff0302e82a5fee691037bdf45
Rhiana Heath <rhiana.heath@reinteractive.net>
2019-03-22 11:22:55 +1100

    adding inline errors to the form inputs

--- app/models/circle_graph.rb
+++ app/models/circle_graph.rb
@@ -1,6 +1,9 @@
 class CircleGraph < ApplicationRecord
   self.table_name = "circle_graphs"
 
-  validates :score, presence: true, numericality: { only_integer: true, less_than_or_equal_to: :total }
-  validates :total, presence: true, numericality: { only_integer: true }
+  validates :score, :total, presence: true
+  validates :score, :total, numericality: { only_integer: true }
+
+  validates :score, numericality: { less_than_or_equal_to: :total }, if: :total
+
 end
--- app/models/gauge_graph.rb
+++ app/models/gauge_graph.rb
@@ -1,7 +1,9 @@
 class GaugeGraph < ApplicationRecord
   self.table_name = "gauge_graphs"
+  
+    validates :score, :total, presence: true
+    validates :score, :total, numericality: { only_integer: true }
 
-  validates :score, presence: true, numericality: { only_integer: true, less_than_or_equal_to: :total }
-  validates :total, presence: true, numericality: { only_integer: true }
+    validates :score, numericality: { less_than_or_equal_to: :total }, if: :total
 
 end
