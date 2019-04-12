0a25504f4ff1f18e01d1c56054d3c66b9c60e472
Rhiana Heath <rhiana.heath@reinteractive.net>
2019-04-12 16:18:03 +1000

    replacing the gauge graph presenter with the gem version

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def show
     @circle = SvgDataPlotterGem::Circle.new({score: 27, total: 100, size: 200})
+    @gauge  = SvgDataPlotterGem::Gauge.new({score: 65, total: 100, size: 180})
   end
 end
